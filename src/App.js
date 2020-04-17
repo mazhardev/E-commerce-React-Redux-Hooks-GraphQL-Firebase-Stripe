64541107bcf928f79d95e1ce5e8081009f4c127f
Muhammad Mazhar <37449942+mazhardev@users.noreply.github.com>
2020-04-17 10:36:40 +0500

    Adding home page with simple layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import HomePage from './components/homepage/HomePage';
 
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return <div className='App'>
+      <HomePage />
+    </div>;
   }
 }
 
