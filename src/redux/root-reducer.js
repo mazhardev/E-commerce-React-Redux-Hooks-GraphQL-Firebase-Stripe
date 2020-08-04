150f1ad80c1439c95b58e27e0805d73fa92be633
Muhammad Mazhar <37449942+mazhardev@users.noreply.github.com>
2020-08-04 15:37:50 +0500

    shop data move to redux store

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,7 +4,8 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
-import directoryReducer  from './directory/directory.reducer'
+import directoryReducer from './directory/directory.reducer'
+import shopReducer from './shop/shop.reducer'
 const persistConfig = {
   key: 'root',
   storage,
@@ -14,7 +15,8 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
-  directory:directoryReducer
+  directory: directoryReducer,
+  shop: shopReducer
 });
 
 export default persistReducer(persistConfig, rootReducer)
